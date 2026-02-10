4d4adae349f24bfe2434fe4304fcc69e4fdac98d
Sherif-Sameh <sherif252525@gmail.com>
2026-02-10 17:28:51 +0100

    Add launch file for Isaac ROS Apriltag detector. Add node for debugging Apriltag detector

---
+++ launch/detector_apriltag.launch.py
@@ -0,0 +1,159 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
+
+
+def launch_setup(context):
+    use_debugger = LaunchConfiguration("use_debugger").perform(context)
+    image_topic_name = LaunchConfiguration("image_topic_name")
+
+    # Launch node based on argument value
+    if use_debugger == "true":
+        return [
+            Node(
+                package="visual_control_pkg",
+                executable="detector_debugger.py",
+                output="screen",
+                remappings=[
+                    ("/image", image_topic_name),
+                    ("/detections", "/detector/tag_detections"),
+                ],
+            )
+        ]
+
+    return []
+
+
+def generate_launch_description():
+    declared_arguments = []
+    # Isaac ROS Apriltag detector arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "size",
+            default_value="0.1",
+            description="The tag edge size in meters, assuming square markers."
+            " Default value is 0.1.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "max_tags",
+            default_value="16",
+            description="The maximum number of tags to be detected. Default value is 16.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tile_size",
+            default_value="4",
+            description="Tile/window size used for adaptive thresholding in pixels."
+            " Default value is 4.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tag_family",
+            default_value="tag36h11",
+            description="Tag family to detect. CUDA backend only supports tag36h11."
+            " CPU and PVA backends support all choices. Default value is tag36h11.",
+            choices=[
+                "tag36h11",
+                "tag16h5",
+                "tag25h9",
+                "tag36h10",
+                "tag36h11",
+                "circle21h7",
+                "circle49h12",
+                "custom48h12",
+                "standard41h12",
+                "standard52h13",
+            ],
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "backends",
+            default_value="CUDA",
+            description="Backend to perform detection with. Default value is CUDA.",
+            choices=[
+                "CUDA",
+                "CPU",
+                "PVA",
+            ],
+        )
+    )
+    # Detector debugger arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_debugger",
+            default_value="false",
+            description="Enable the Apriltag detector debugger node for publishing"
+            " additional visualizations. Default value is false.",
+        )
+    )
+    # Topic name arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "image_topic_name",
+            default_value="/image",
+            description="Input image (sensor_msgs/Image) topic name to use for detector.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "camera_info_topic_name",
+            default_value="/camera_info",
+            description="Camera info (sensor_msgs/CameraInfo) topic name to use for detector.",
+        )
+    )
+
+    # Initialize Arguments
+    # Isaac ROS Apriltag detector arguments
+    size = LaunchConfiguration("size")
+    max_tags = LaunchConfiguration("max_tags")
+    tile_size = LaunchConfiguration("tile_size")
+    tag_family = LaunchConfiguration("tag_family")
+    backends = LaunchConfiguration("backends")
+    # Topic name arguments
+    image_topic_name = LaunchConfiguration("image_topic_name")
+    camera_info_topic_name = LaunchConfiguration("camera_info_topic_name")
+
+    # Apriltag node and node container
+    apriltag_node = ComposableNode(
+        package="isaac_ros_apriltag",
+        plugin="nvidia::isaac_ros::apriltag::AprilTagNode",
+        name="apriltag",
+        parameters=[
+            {
+                "size": size,
+                "max_tags": max_tags,
+                "tile_size": tile_size,
+                "tag_family": tag_family,
+                "backends": backends,
+            }
+        ],
+        remappings=[
+            ("/image", image_topic_name),
+            ("/camera_info", camera_info_topic_name),
+            ("/tag_detections", "/detector/tag_detections"),
+        ],
+    )
+
+    apriltag_container = ComposableNodeContainer(
+        package="rclcpp_components",
+        name="apriltag_container",
+        namespace="",
+        executable="component_container_mt",
+        composable_node_descriptions=[
+            apriltag_node,
+        ],
+        output="screen",
+    )
+
+    # Combine opaque function with launch description
+    op_func = OpaqueFunction(function=launch_setup)
+    ld = LaunchDescription(declared_arguments + [apriltag_container])
+    ld.add_action(op_func)
+    return ld
---
+++ scripts/detector_debugger.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+"""
+Debugging node for the Apriltag detector from Isaac ROS Apriltag.
+"""
+
+import cv2
+import rclpy
+from cv_bridge import CvBridge
+from isaac_ros_apriltag_interfaces.msg import AprilTagDetection, AprilTagDetectionArray
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+
+
+class DetectorDebugger(Node):
+    """Debugging node for April tag detector from Isaac ROS Apriltag.
+
+    This node is used to publish messages based on the detector's outputs which could be used for
+    debugging them. Given the AprilTagDetectionArray message from the detector, the debugger will
+    publish a modified version of the input image with the tag's center, border adn ID overlayed
+    on top.
+    """
+
+    def __init__(self):
+        super().__init__("detector_debugger")
+
+        # Publishers
+        self._pub_img = self.create_publisher(Image, "/detector_debugger/image", 0)
+
+        # Subscribers
+        self._sub_img = self.create_subscription(
+            Image, "/image", self.image_callback, 0
+        )
+        self._sub_dtn = self.create_subscription(
+            AprilTagDetectionArray, "/detections", self.detection_callback, 0
+        )
+
+        # Other attributes
+        self._bridge = CvBridge()
+        self._img = None
+
+    def _publish_tag_image(self, msg: AprilTagDetectionArray) -> None:
+        """Publish stored input image with tags overlayed on top.
+
+        Args:
+            msg: AprilTagDetectionArray message containing the relative poses of each tag.
+        """
+        # Constants
+        RECT_COLOR, RECT_THICKNESS = (255, 0, 0), 2  # red and thickness in pixels
+        CIRC_COLOR, CIRC_RADIUS = (0, 0, 255), 5  # blue and radius in pixels
+        FONT_COLOR, FONT_THICKNESS = (0, 255, 0), 2  # green and thickness in pixels
+        FONT_FACE, FONT_SCALE = cv2.FONT_HERSHEY_SIMPLEX, 1.0  # font type and size
+        CENTER_OFFSET = (35, 35)
+
+        # Format image
+        detections: list[AprilTagDetection] = msg.detections  # for type inference
+        for dtn in detections:
+            # Draw circle at tag center
+            self._img = cv2.circle(
+                self._img,
+                center=(int(dtn.center.x), int(dtn.center.y)),
+                radius=CIRC_RADIUS,
+                color=CIRC_COLOR,
+                thickness=-1,  # filled
+            )
+
+            # Draw rectangle around tag boundary
+            self._img = cv2.line(
+                self._img,
+                pt1=(int(dtn.corners[0].x), int(dtn.corners[0].y)),
+                pt2=(int(dtn.corners[1].x), int(dtn.corners[1].y)),
+                color=RECT_COLOR,
+                thickness=RECT_THICKNESS,
+            )
+            self._img = cv2.line(
+                self._img,
+                pt1=(int(dtn.corners[1].x), int(dtn.corners[1].y)),
+                pt2=(int(dtn.corners[2].x), int(dtn.corners[2].y)),
+                color=RECT_COLOR,
+                thickness=RECT_THICKNESS,
+            )
+            self._img = cv2.line(
+                self._img,
+                pt1=(int(dtn.corners[2].x), int(dtn.corners[2].y)),
+                pt2=(int(dtn.corners[3].x), int(dtn.corners[3].y)),
+                color=RECT_COLOR,
+                thickness=RECT_THICKNESS,
+            )
+            self._img = cv2.line(
+                self._img,
+                pt1=(int(dtn.corners[3].x), int(dtn.corners[3].y)),
+                pt2=(int(dtn.corners[0].x), int(dtn.corners[0].y)),
+                color=RECT_COLOR,
+                thickness=RECT_THICKNESS,
+            )
+
+            # Draw tag's ID on top of it
+            self._img = cv2.putText(
+                self._img,
+                text=f"ID:{dtn.id}",
+                org=(
+                    int(dtn.center.x) - CENTER_OFFSET[0],
+                    int(dtn.center.y) - CENTER_OFFSET[1],
+                ),
+                fontFace=FONT_FACE,
+                fontScale=FONT_SCALE,
+                color=FONT_COLOR,
+                thickness=FONT_THICKNESS,
+            )
+
+        # Publish modified image
+        msg = self._bridge.cv2_to_imgmsg(self._img, encoding="rgb8")
+        msg.header.stamp = self.get_clock().now().to_msg()
+        self._pub_img.publish(msg)
+
+    def image_callback(self, msg: Image) -> None:
+        """Callback function for input image.
+
+        Stores input image for use in detection callback.
+
+        Args:
+            msg: sensor_msgs/Image input messsage.
+        """
+        self._img = self._bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
+
+    def detection_callback(self, msg: AprilTagDetectionArray) -> None:
+        """Callback function for the Apriltag detections.
+
+        Publishes modified version of stored input image with tags overlayed on top of it.
+
+        Args:
+            msg: AprilTagDetectionArray message containing the relative poses of each tag.
+        """
+        # Publish modified image if input image is available
+        if self._img is not None:
+            self._publish_tag_image(msg)
+
+
+def main(args=None):
+    # Initialize ROS
+    rclpy.init(args=args)
+
+    # Create node and spin indefinitely
+    detector_debugger = DetectorDebugger()
+    rclpy.spin(detector_debugger)
+
+    # Destroy node explicitly
+    detector_debugger.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
