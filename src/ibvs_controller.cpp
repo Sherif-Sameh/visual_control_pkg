bacaf43cb2573069b6a3783d82f44e0532f53d02
Sherif-Sameh <sherif252525@gmail.com>
2026-02-21 23:52:51 +0100

    Add header and implementation for IBVS controller. Update timeout threshold for PBVS controller

--- src/ibvs_controller.cpp
+++ src/ibvs_controller.cpp
@@ -1,6 +1,6 @@
-#include "pbvs_controller.hpp"
+#include "ibvs_controller.hpp"
 
-PbvsController::PbvsController() : Node("pbvs_controller")
+IbvsController::IbvsController() : Node("ibvs_controller")
 {
     // Declare ROS parameters
     this->declare_parameter("verbose", rclcpp::PARAMETER_BOOL);
@@ -9,6 +9,7 @@ PbvsController::PbvsController() : Node("pbvs_controller")
     this->declare_parameter("frame.ee_frame", rclcpp::PARAMETER_STRING);
     this->declare_parameter("frame.cam_frame", rclcpp::PARAMETER_STRING);
     this->declare_parameter("tag.tag_family", rclcpp::PARAMETER_STRING);
+    this->declare_parameter("tag.tag_size", rclcpp::PARAMETER_DOUBLE);
     this->declare_parameter("tag.tag_ids", rclcpp::PARAMETER_INTEGER_ARRAY);
     this->declare_parameter("ik.eps", rclcpp::PARAMETER_DOUBLE);
     this->declare_parameter("ik.lambda", rclcpp::PARAMETER_DOUBLE);
@@ -19,46 +20,50 @@ PbvsController::PbvsController() : Node("pbvs_controller")
     this->declare_parameter("robot.max_vel_sf", rclcpp::PARAMETER_DOUBLE);
     this->declare_parameter("robot.max_qdot", rclcpp::PARAMETER_DOUBLE_ARRAY);
     this->declare_parameter("ctrl.conv_ttol", rclcpp::PARAMETER_DOUBLE);
-    this->declare_parameter("ctrl.conv_rtol", rclcpp::PARAMETER_DOUBLE);
     this->declare_parameter("ctrl.lambda", rclcpp::PARAMETER_DOUBLE_ARRAY);
 
     // Initialize non-ROS class attributes
+    m_is_cam_init = false;
     m_base_frame = this->get_parameter("frame.base_frame").as_string();
     m_ee_frame = this->get_parameter("frame.ee_frame").as_string();
     m_cam_frame = this->get_parameter("frame.cam_frame").as_string();
     m_tag_family = this->get_parameter("tag.tag_family").as_string();
+    double tag_size = this->get_parameter("tag.tag_size").as_double();
     std::vector<int64_t> tag_ids = this->get_parameter("tag.tag_ids").as_integer_array();
     std::transform(tag_ids.begin(), tag_ids.end(), std::back_inserter(m_tag_ids),
                    [](int64_t id) { return static_cast<int>(id); });
+    m_points[0].setWorldCoordinates(-tag_size / 2, -tag_size / 2, 0);
+    m_points[1].setWorldCoordinates(tag_size / 2, -tag_size / 2, 0);
+    m_points[2].setWorldCoordinates(tag_size / 2, tag_size / 2, 0);
+    m_points[3].setWorldCoordinates(-tag_size / 2, tag_size / 2, 0);
     for (const int id : m_tag_ids)
     {
-        m_t.insert({id, vpFeatureTranslation(vpFeatureTranslation::cdMc)});
-        m_tu.insert({id, vpFeatureThetaU(vpFeatureThetaU::cdRc)});
-        m_t[id].buildFrom(vpHomogeneousMatrix());  // set error to zero
-        m_tu[id].buildFrom(vpHomogeneousMatrix()); // set error to zero
+        m_p.insert({id, std::array<vpFeaturePoint, 4>()});
     }
     init_robot_and_controller();
 
     // Initialize ROS attributes
     m_pub_perr =
-        this->create_publisher<geometry_msgs::msg::PoseArray>("/pbvs_controller/pose_error", 10);
+        this->create_publisher<geometry_msgs::msg::PoseArray>("/ibvs_controller/pose_error", 10);
     m_pub_traj = this->create_publisher<trajectory_msgs::msg::JointTrajectory>(
         "/joint_trajectory_controller/joint_trajectory", 0);
     m_sub_js = this->create_subscription<sensor_msgs::msg::JointState>(
-        "/joint_states", 0, std::bind(&PbvsController::callback_js, this, _1));
+        "/joint_states", 0, std::bind(&IbvsController::callback_js, this, _1));
+    m_sub_cam_info = this->create_subscription<sensor_msgs::msg::CameraInfo>(
+        "/camera_info", 0, std::bind(&IbvsController::callback_cam_info, this, _1));
     m_sub_tag = this->create_subscription<AprilTagDetectionArray>(
-        "/detections", 0, std::bind(&PbvsController::callback_tag, this, _1));
+        "/detections", 0, std::bind(&IbvsController::callback_tag, this, _1));
     m_tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
     m_tf_listener = std::make_shared<tf2_ros::TransformListener>(*m_tf_buffer);
 }
 
-PbvsController::~PbvsController()
+IbvsController::~IbvsController()
 {
     std::vector<double> qdot_zero(m_robot.getNumDofs(), 0.0);
     publish_traj(qdot_zero);
 }
 
-void PbvsController::post_init()
+void IbvsController::post_init()
 {
     constexpr int max_trials = 20;
     constexpr auto sleep_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(0.5s);
@@ -76,26 +81,22 @@ void PbvsController::post_init()
     rclcpp::shutdown(nullptr, "Robot initialization failed.");
 }
 
-void PbvsController::publish_perr(const std::vector<double> &perr)
+void IbvsController::publish_perr(const std::vector<double> &perr)
 {
     geometry_msgs::msg::PoseArray msg;
     msg.header.stamp = this->get_clock()->now();
 
-    std::size_t n_poses = perr.size() / 6;
+    std::size_t n_poses = perr.size() / 2;
     msg.poses.resize(n_poses);
     for (std::size_t i = 0; i < n_poses; i++)
     {
-        msg.poses[i].position.x = perr[6 * i];
-        msg.poses[i].position.y = perr[6 * i + 1];
-        msg.poses[i].position.z = perr[6 * i + 2];
-
-        msg.poses[i].orientation =
-            xyz_aa_to_gm_quat(perr[6 * i + 3], perr[6 * i + 4], perr[6 * i + 5]);
+        msg.poses[i].position.x = perr[2 * i];
+        msg.poses[i].position.y = perr[2 * i + 1];
     }
     m_pub_perr->publish(msg);
 }
 
-void PbvsController::publish_traj(const std::vector<double> &qdot)
+void IbvsController::publish_traj(const std::vector<double> &qdot)
 {
     trajectory_msgs::msg::JointTrajectory msg;
     msg.header.stamp = this->get_clock()->now();
@@ -109,33 +110,64 @@ void PbvsController::publish_traj(const std::vector<double> &qdot)
     m_pub_traj->publish(msg);
 }
 
-void PbvsController::callback_js(const sensor_msgs::msg::JointState::SharedPtr msg)
+void IbvsController::callback_js(const sensor_msgs::msg::JointState::SharedPtr msg)
 {
     m_joint_names = msg->name;
     m_robot.setJointPosition(msg->position);
 }
 
-void PbvsController::callback_tag(const AprilTagDetectionArray::SharedPtr msg)
+void IbvsController::callback_cam_info(const sensor_msgs::msg::CameraInfo::SharedPtr msg)
 {
-    if (m_cdMo.size() > 0)
+    if (!m_is_cam_init)
     {
-        // Update tag transformations
-        for (auto const &tag : m_cdMo)
+        // K = [fx, 0, cx, 0, fy, cy, 0, 0, 1]
+        m_cam_params.initPersProjWithoutDistortion(msg->k[0], msg->k[4], msg->k[2], msg->k[5]);
+        m_is_cam_init = true;
+    }
+}
+
+void IbvsController::callback_tag(const AprilTagDetectionArray::SharedPtr msg)
+{
+    if (m_is_cam_init && m_pd.size() > 0)
+    {
+        // Update tag feature points
+        std::vector<int> valid_ids, invalid_ids;
+        for (auto const &tag : m_pd)
         {
             const int id = tag.first;
-            vpHomogeneousMatrix cdMc; // set error to identity
             auto it = std::find_if(msg->detections.cbegin(), msg->detections.cend(),
                                    [id](const AprilTagDetection &dtn) { return dtn.id == id; });
             if (it != msg->detections.cend())
             {
+                valid_ids.push_back(id);
                 vpHomogeneousMatrix cMo = gm_pose_to_vp_hmatrix((*it).pose.pose.pose);
-                vpHomogeneousMatrix cdMc_tmp = tag.second * cMo.inverse();
-                if (cdMc_tmp.getTranslationVector().sumSquare() > m_conv_eps.first ||
-                    cdMc_tmp.getThetaUVector().sumSquare() > m_conv_eps.second)
-                    cdMc = cdMc_tmp;
+                for (std::size_t i = 0; i < 4; i++)
+                {
+                    vpImagePoint corner((*it).corners[i].y, (*it).corners[i].x);
+                    vpFeatureBuilder::create(m_p[id][i], m_cam_params, corner);
+                    vpColVector cP;
+                    m_points[i].changeFrame(cMo, cP);
+                    m_p[id][i].set_Z(cP[2]);
+                }
             }
-            m_t[id].buildFrom(cdMc);
-            m_tu[id].buildFrom(cdMc);
+            else
+            {
+                invalid_ids.push_back(id);
+            }
+        }
+
+        // Replace invalid tags with valid duplicates if available
+        if (valid_ids.size() == 0)
+        {
+            return;
+        }
+        if (invalid_ids.size() > 0)
+        {
+            for (const int id : invalid_ids)
+            {
+                m_p[id] = m_p[valid_ids[0]];
+                m_pd[id] = m_pd[valid_ids[0]];
+            }
         }
 
         // Get end-effector pose relative to robot base from TF Tree
@@ -147,19 +179,24 @@ void PbvsController::callback_tag(const AprilTagDetectionArray::SharedPtr msg)
 
         // Compute camera velocity and convert to joint velocities
         vpColVector v_c = m_lambda.hadamard(m_controller.computeControlLaw());
-        std::vector<double> qdot = m_robot.computeJointVelocity(fMe, v_c);
+        vpColVector perr = m_controller.getError();
+        std::vector<double> qdot(m_robot.getNumDofs(), 0.0);
+        if (perr.sumSquare() > m_conv_eps)
+        {
+            qdot = m_robot.computeJointVelocity(fMe, v_c);
+        }
         publish_traj(qdot);
-        publish_perr(m_controller.getError().toStdVector()); // log errors
+        publish_perr(perr.toStdVector()); // log errors
     }
 
     // Check for desired pose updates
     get_desired_poses();
 }
 
-void PbvsController::get_desired_poses()
+void IbvsController::get_desired_poses()
 {
     // Query transforms for tag transforms relative to desired camera frame
-    // and update the corresponding homogeneous transforms
+    // and update the corresponding desired feature points
     for (const int id : m_tag_ids)
     {
         std::string tag_id_frame = m_tag_family + ":" + std::to_string(id);
@@ -167,12 +204,27 @@ void PbvsController::get_desired_poses()
         vpHomogeneousMatrix cdMo_id;
         if (lookup_transform(cam_d_frame, tag_id_frame, cdMo_id))
         {
-            m_cdMo[id] = cdMo_id;
+            // Initialize new tag for controller to track
+            if (m_pd.find(id) == m_pd.end())
+            {
+                m_pd.insert({id, std::array<vpFeaturePoint, 4>()});
+                for (std::size_t i = 0; i < 4; i++)
+                {
+                    m_controller.addFeature(m_p[id][i], m_pd[id][i]);
+                }
+            }
+
+            // Update tag desired feature points
+            for (std::size_t i = 0; i < 4; i++)
+            {
+                m_points[i].track(cdMo_id);
+                vpFeatureBuilder::create(m_pd[id][i], m_points[i]);
+            }
         }
     }
 }
 
-bool PbvsController::lookup_transform(const std::string &target_frame,
+bool IbvsController::lookup_transform(const std::string &target_frame,
                                       const std::string &source_frame, vpHomogeneousMatrix &t)
 {
     geometry_msgs::msg::TransformStamped t_gm;
@@ -191,7 +243,7 @@ bool PbvsController::lookup_transform(const std::string &target_frame,
     return true;
 }
 
-void PbvsController::init_robot_and_controller()
+void IbvsController::init_robot_and_controller()
 {
     bool verbose = this->get_parameter("verbose").as_bool();
     std::string robot_description = this->get_parameter("robot_description").as_string();
@@ -222,25 +274,19 @@ void PbvsController::init_robot_and_controller()
     m_robot.init(robot_description);
 
     // Initialize controller
-    m_conv_eps.first = std::pow(this->get_parameter("ctrl.conv_ttol").as_double(), 2);
-    m_conv_eps.second = std::pow(this->get_parameter("ctrl.conv_rtol").as_double(), 2);
+    m_conv_eps = std::pow(this->get_parameter("ctrl.conv_ttol").as_double(), 2);
     m_lambda = this->get_parameter("ctrl.lambda").as_double_array();
     m_controller.setLambda(1.0);
     m_controller.setServo(vpServo::EYEINHAND_CAMERA);
     m_controller.setInteractionMatrixType(vpServo::CURRENT);
-    for (const int id : m_tag_ids)
-    {
-        m_controller.addFeature(m_t[id]);
-        m_controller.addFeature(m_tu[id]);
-    }
 }
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    auto pbvs_controller = std::make_shared<PbvsController>();
-    pbvs_controller->post_init();
-    rclcpp::spin(pbvs_controller);
+    auto ibvs_controller = std::make_shared<IbvsController>();
+    ibvs_controller->post_init();
+    rclcpp::spin(ibvs_controller);
     rclcpp::shutdown();
     return 0;
 }
---
+++ src/ibvs_controller.hpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <visp3/core/vpCameraParameters.h>
+#include <visp3/visual_features/vpFeatureBuilder.h>
+#include <visp3/visual_features/vpFeaturePoint.h>
+#include <visp3/vs/vpServo.h>
+
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "isaac_ros_apriltag_interfaces/msg/april_tag_detection.hpp"
+#include "isaac_ros_apriltag_interfaces/msg/april_tag_detection_array.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "tf2/exceptions.hpp"
+#include "tf2_ros/buffer.hpp"
+#include "tf2_ros/transform_listener.hpp"
+#include "trajectory_msgs/msg/joint_trajectory.hpp"
+
+#include "utils/geometry.hpp"
+#include "vpRobotRos.hpp"
+
+using isaac_ros_apriltag_interfaces::msg::AprilTagDetection;
+using isaac_ros_apriltag_interfaces::msg::AprilTagDetectionArray;
+using std::placeholders::_1;
+using vc::visp::vpRobotRos;
+using namespace std::chrono_literals;
+
+class IbvsController : public rclcpp::Node
+{
+public:
+    IbvsController();
+    ~IbvsController();
+    void post_init();
+
+private:
+    void publish_perr(const std::vector<double> &perr);
+    void publish_traj(const std::vector<double> &qdot);
+    void callback_js(const sensor_msgs::msg::JointState::SharedPtr msg);
+    void callback_tag(const AprilTagDetectionArray::SharedPtr msg);
+    void callback_cam_info(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
+    void get_desired_poses();
+    bool lookup_transform(const std::string &target_frame, const std::string &source_frame,
+                          vpHomogeneousMatrix &t);
+    void init_robot_and_controller();
+
+private:
+    // General Attributes
+    bool m_is_cam_init;
+    std::string m_base_frame;
+    std::string m_ee_frame;
+    std::string m_cam_frame;
+    std::string m_tag_family;
+    std::vector<int> m_tag_ids;
+    std::vector<std::string> m_joint_names;
+
+    // ViSP Attributes
+    double m_conv_eps;
+    std::array<vpPoint, 4> m_points;
+    std::unordered_map<int, std::array<vpFeaturePoint, 4>> m_p;
+    std::unordered_map<int, std::array<vpFeaturePoint, 4>> m_pd;
+    vpCameraParameters m_cam_params;
+    vpColVector m_lambda;
+    vpRobotRos m_robot;
+    vpServo m_controller;
+
+    // ROS Attributes
+    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr m_pub_perr{nullptr};
+    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr m_pub_traj{nullptr};
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr m_sub_js{nullptr};
+    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr m_sub_cam_info{nullptr};
+    rclcpp::Subscription<AprilTagDetectionArray>::SharedPtr m_sub_tag{nullptr};
+    std::shared_ptr<tf2_ros::TransformListener> m_tf_listener{nullptr};
+    std::unique_ptr<tf2_ros::Buffer> m_tf_buffer;
+};
