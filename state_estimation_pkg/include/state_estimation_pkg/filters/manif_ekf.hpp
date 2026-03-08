8dbaf1d8eeadd1276bd2224f31b5b8e79f9df618
Sherif-Sameh <sherif252525@gmail.com>
2026-03-08 02:22:35 +0100

    Add generic EKF template for Lie Groups using manif

---
+++ state_estimation_pkg/include/state_estimation_pkg/filters/manif_ekf.hpp
@@ -0,0 +1,173 @@
+/*
+ * Description:
+ * Extened Kalman Filter (EKF) implementation using the manif library for Lie groups.
+ * For more on manif, check out the GitHub page: https://github.com/artivis/manif
+ */
+
+#ifndef MANIF_EKF
+#define MANIF_EKF
+
+#include <algorithm>
+#include <cassert>
+
+#include <manif/manif.h>
+
+#include "actions/unit.hpp"
+#include "measurements/unit.hpp"
+
+namespace se
+{
+    template <class _Group, class _Action = ActionUnit<_Group>,
+              class _Measure = MeasurementUnit<_Group>>
+    class ManifEKF
+    {
+    public:
+        static constexpr int xDoF = manif::LieGroupBase<_Group>::DoF;
+        static constexpr int uDoF = ActionBase<_Group, _Action>::DoF;
+        static constexpr int yDoF = MeasurementBase<_Group, _Measure>::DoF;
+
+        using Scalar = typename manif::LieGroupBase<_Group>::Scalar;
+        using State = typename manif::LieGroupBase<_Group>::LieGroup;
+        using Tangent = typename manif::LieGroupBase<_Group>::Tangent;
+        using Jacobian = typename manif::LieGroupBase<_Group>::Jacobian;
+        using Covariance = Eigen::Matrix<Scalar, xDoF, xDoF>;
+        using KalmanGain = Eigen::Matrix<Scalar, xDoF, yDoF>;
+
+        using Action = typename ActionBase<_Group, _Action>::Action;
+        using JacobianA = typename ActionBase<_Group, _Action>::Jacobian;
+        using CovarianceA = Eigen::Matrix<Scalar, uDoF, uDoF>;
+
+        using Measurement = typename MeasurementBase<_Group, _Measure>::Measurement;
+        using DeltaMeasurement = typename MeasurementBase<_Group, _Measure>::DeltaMeasurement;
+        using JacobianM = typename MeasurementBase<_Group, _Measure>::Jacobian;
+        using CovarianceM = Eigen::Matrix<Scalar, yDoF, yDoF>;
+
+    protected:
+        static constexpr _Action u_default = ActionUnit<_Group>();
+        static constexpr _Measure h_default = MeasurementUnit<_Group>();
+
+    public:
+        ManifEKF();
+        ManifEKF(const Covariance &P0, const CovarianceA &Q, const CovarianceM &R);
+
+        auto getProcessCovariance() const -> CovarianceA;
+        auto getMeasurementCovariance() const -> CovarianceM;
+        void setState(const State &x0);
+        void setErrorCovariance(const Covariance &P0);
+        void setProcessCovariance(const CovarianceA &Q);
+        void setMeasurementCovariance(const CovarianceM &R);
+
+        void predict(const Action &u, State &x_out, Covariance &P_out,
+                     const _Action &u_func = u_default);
+        void update(const Measurement &y, State &x_out, Covariance &P_outm,
+                    const _Measure &h_func = h_default);
+
+    protected:
+        template <typename _Covariance>
+        bool checkCovarianceMatrix(const _Covariance &A);
+
+    protected:
+        State m_x;
+        Covariance m_P;
+        CovarianceA m_Q;
+        CovarianceM m_R;
+    };
+
+    // Definitions
+
+    template <class _Group, class _Action, class _Measure>
+    ManifEKF<_Group, _Action, _Measure>::ManifEKF()
+        : m_x(State::Identity()), m_P(Covariance::Zero()), m_Q(CovarianceA::Identity()),
+          m_R(CovarianceM::Identity())
+    {
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    ManifEKF<_Group, _Action, _Measure>::ManifEKF(const Covariance &P0, const CovarianceA &Q,
+                                                  const CovarianceM &R)
+        : m_x(State::Identity()), m_P(P0), m_Q(Q), m_R(R)
+    {
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    auto ManifEKF<_Group, _Action, _Measure>::getProcessCovariance() const -> CovarianceA
+    {
+        return m_Q;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    auto ManifEKF<_Group, _Action, _Measure>::getMeasurementCovariance() const -> CovarianceM
+    {
+        return m_R;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    void ManifEKF<_Group, _Action, _Measure>::setState(const State &x0)
+    {
+        m_x = x0;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    void ManifEKF<_Group, _Action, _Measure>::setErrorCovariance(const Covariance &P0)
+    {
+        assert(checkCovarianceMatrix(P0));
+        m_P = P0;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    void ManifEKF<_Group, _Action, _Measure>::setProcessCovariance(const CovarianceA &Q)
+    {
+        assert(checkCovarianceMatrix(Q));
+        m_Q = Q;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    void ManifEKF<_Group, _Action, _Measure>::setMeasurementCovariance(const CovarianceM &R)
+    {
+        assert(checkCovarianceMatrix(R));
+        m_R = R;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    void ManifEKF<_Group, _Action, _Measure>::predict(const Action &u, State &x_out,
+                                                      Covariance &P_out, const _Action &u_func)
+    {
+        JacobianA J_uw;
+        Tangent tan = u_func(m_x, u, J_uw);
+
+        Jacobian J_fx, J_fu;
+        m_x = m_x.plus(tan, J_fx, J_fu);
+        m_P =
+            J_fx * m_P * J_fx.transpose() + J_fu * J_uw * m_Q * J_uw.transpose() * J_fu.transpose();
+        x_out = m_x;
+        P_out = m_P;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    void ManifEKF<_Group, _Action, _Measure>::update(const Measurement &y, State &x_out,
+                                                     Covariance &P_out, const _Measure &h_func)
+    {
+        JacobianM J_hx;
+        Measurement y_exp = h_func(m_x, J_hx);
+
+        DeltaMeasurement z = y - y_exp;
+        CovarianceM Z = J_hx * m_P * J_hx.transpose() + m_R;
+        KalmanGain K = m_P * J_hx.transpose() * Z.inverse();
+        m_x = m_x.plus(K * z);
+        m_P = m_P - K * Z * K.transpose();
+        x_out = m_x;
+        P_out = m_P;
+    }
+
+    template <class _Group, class _Action, class _Measure>
+    template <typename _Covariance>
+    bool ManifEKF<_Group, _Action, _Measure>::checkCovarianceMatrix(const _Covariance &A)
+    {
+        assert(A.isApprox(A.transpose()));
+        assert(
+            std::all_of(A.diagonal().begin(), A.diagonal().end(), [](Scalar x) { return x >= 0; }));
+        return true;
+    }
+} // namespace se
+
+#endif
