77b743c78129070944c79fadb275eb02168af548
Sherif-Sameh <sherif252525@gmail.com>
2026-03-19 19:20:25 +0100

    Update script for charuco generation

--- vc_core/vc_core/utils/marker_gen/charuco.py
+++ vc_core/vc_core/utils/marker_gen/charuco.py
@@ -5,7 +5,13 @@ import fire
 
 
 def main(
-    xs: int, ys: int, sq_len: float, mk_len: float, dict: str = "DICT_5X5_50", dpi: int = 300
+    xs: int,
+    ys: int,
+    sq_len: float,
+    mk_len: float,
+    dict: str = "DICT_5X5_50",
+    border: int = 0,
+    dpi: int = 300,
 ) -> None:
     """Generate image of ChArUco board with the given specs.
 
@@ -15,12 +21,13 @@ def main(
         sq_len: Length of square sides of chessboard.
         mk_len: Length of square sides of ArUco markers.
         dict: ArUco dictionary to use.
+        border: Border bits for generated image.
         dpi: Output DPI for generated image (affects resolution).
     """
     assert xs > 0 and ys > 0
     assert sq_len > 0 and mk_len > 0 and sq_len > mk_len
     assert hasattr(cv2.aruco, dict)
-    assert dpi > 0
+    assert border >= 0 and dpi > 0
     # create ChArUco board
     dict_id = getattr(cv2.aruco, dict)
     aruco_dict = cv2.aruco.getPredefinedDictionary(dict_id)
@@ -29,9 +36,9 @@ def main(
     # render board
     INCHES_PER_METER = 39.3701
     width_m, height_m = xs * sq_len, ys * sq_len
-    width_px = int(width_m * INCHES_PER_METER * dpi)
-    height_px = int(height_m * INCHES_PER_METER * dpi)
-    img = board.generateImage((width_px, height_px), 10, 100)
+    width_px = int(width_m * INCHES_PER_METER * dpi) + 2 * border
+    height_px = int(height_m * INCHES_PER_METER * dpi) + 2 * border
+    img = board.generateImage((width_px, height_px), 0, border)
 
     # save to png file
     name = f"charuco_{xs}x{ys}_{sq_len:.2f}_{mk_len:.2f}_{dict}"
