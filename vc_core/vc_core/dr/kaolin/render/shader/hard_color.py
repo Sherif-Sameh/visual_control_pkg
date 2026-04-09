1cb4455e8dc772b985e6d18fb09bcf791eea1eab
Sherif-Sameh <sherif252525@gmail.com>
2026-04-09 20:10:24 +0200

    Add diffuse and specular hard color shader for Kaolin. Add batched version of generate_pinhole_rays to kaolin utils

--- vc_core/vc_core/dr/kaolin/render/shader/hard_color.py
+++ vc_core/vc_core/dr/kaolin/render/shader/hard_color.py
@@ -6,6 +6,8 @@ import kaolin as kal
 import torch
 from kaolin.render.lighting import SgLightingParameters
 
+from vc_core.dr.kaolin.utils import generate_pinhole_rays_batched
+
 from .base import Shader
 
 if TYPE_CHECKING:
@@ -102,7 +104,7 @@ class HardColorDiffuseSH9Shader(Shader):
     Args:
         azimuth: Optional azimuth angles for light direction. Default value is `None`.
         elevation: Optional elevation angles for light direction. Default value is `None`.
-        direction: Optioanl unit vectors for light direction. Default value is `None`.
+        direction: Optional unit vectors for light direction. Default value is `None`.
         degrees: Whether the angles are specified in degrees or radians.
         raw_texture: Whether mesh texture is uint8 (H, W, 3) and needs conversion or is already
             processed. If `False`, the texture must be float [0, 1] (3, H, W). Default value is
@@ -133,7 +135,7 @@ class HardColorDiffuseSH9Shader(Shader):
             f"Light parameters must be 1-dim, got {self._direction.ndim}."
         )
         self._get_albedo = _build_get_albedo(raw_texture, uvs_origin)
-        self._pre_hook = self._pre_hook_fn
+        self._pre_hook = _register_uvs_and_normals
         self._feature_dim = 5
 
     def forward(self, fragments: Fragments, mesh: SurfaceMesh, **kwargs) -> Tensor:
@@ -168,11 +170,6 @@ class HardColorDiffuseSH9Shader(Shader):
         self._direction = self._direction.to(device=device)
         return self
 
-    @staticmethod
-    def _pre_hook_fn(face_vertices_camera: Tensor, mesh: SurfaceMesh) -> Tensor:
-        """Pre-hook function for hard color diffuse shader to register mesh uvs and normals."""
-        return torch.cat([mesh.face_uvs, mesh.face_normals], dim=-1)
-
 
 class HardColorDiffuseSGFittedShader(Shader):
     """Hard color shader considering only diffuse lighting for Kaolin rendering.
@@ -194,7 +191,7 @@ class HardColorDiffuseSGFittedShader(Shader):
         azimuth: Optional azimuth angles for SG light directions. Default value is `None`.
         elevation: Optional elevation angles for SG light directions. Default value is `None`.
         degrees: Whether the angles are specified in degrees or radians.
-        lights: SG lighting parameters. Default value is `None`.
+        lights: Optional SG lighting parameters. Default value is `None`.
         raw_texture: Whether mesh texture is uint8 (H, W, 3) and needs conversion or is already
             processed. If `False`, the texture must be float [0, 1] (3, H, W). Default value is
             `True`.
@@ -223,7 +220,7 @@ class HardColorDiffuseSGFittedShader(Shader):
             lights = SgLightingParameters(lights.amplitude, direction, lights.sharpness)
         self._lights = lights
         self._get_albedo = _build_get_albedo(raw_texture, uvs_origin)
-        self._pre_hook = self._pre_hook_fn
+        self._pre_hook = _register_uvs_and_normals
         self._feature_dim = 5
 
     def forward(self, fragments: Fragments, mesh: SurfaceMesh, **kwargs) -> Tensor:
@@ -258,10 +255,128 @@ class HardColorDiffuseSGFittedShader(Shader):
 
     __call__ = forward
 
-    @staticmethod
-    def _pre_hook_fn(face_vertices_camera: Tensor, mesh: SurfaceMesh) -> Tensor:
-        """Pre-hook function for hard color diffuse shader to register mesh uvs and normals."""
-        return torch.cat([mesh.face_uvs, mesh.face_normals], dim=-1)
+
+class HardColorSpecularSGFittedShader(Shader):
+    """Hard color shader considering diffuse and specular lighting for Kaolin rendering.
+
+    Light is modeled through fitted approximation of Spherical Gaussians and is parameterized
+    by its direction vector in Cartesian space and SG parameters. Material properties outside the
+    diffuse albedo include the specular albedo and material roughness of the mesh. Both of these
+    can be passed during initialization or forwad passes. For more on the fitted SG-based lighting
+    model, refer to `kaolin.render.lighting.sg_diffuse_fitted`. For more on the calculation of the
+    specular lighting component, refer to `kaolin.render.lighting.sg_warp_specular_term`.
+
+    Since the shader is hard, gradients can only propagate from the foreground pixels.
+
+    For efficiency, we restrict ourselves to meshes with a single material per mesh instance.
+    That is `mesh.materials[0][0]["map_Kd"]`. Mesh specular albedo and surface roughness are
+    assumed to be global properties, not spatially-varying. Also, the light is assumed to be
+    the same across all mesh instances but can be modelled with multiple SGs.
+
+    The shader's pre-hook function must be registered with the rasterizer to enable uv
+    rasterization outputs.
+
+    Args:
+        azimuth: Optional azimuth angles for SG light directions. Default value is `None`.
+        elevation: Optional elevation angles for SG light directions. Default value is `None`.
+        degrees: Whether the angles are specified in degrees or radians.
+        spec_albedo: Optional specular albedo. Default value is `None`.
+        roughness: Optional material roughness. Default value is `None`.
+        cameras: Optional batched cameras. Default value is `None`.
+        lights: Optional SG lighting parameters. Default value is `None`.
+        raw_texture: Whether mesh texture is uint8 (H, W, 3) and needs conversion or is already
+            processed. If `False`, the texture must be float [0, 1] (3, H, W). Default value is
+            `True`.
+        uvs_origin: Whether the origin of mesh uvs follows the OpenGL (bottom-left) or Kaolin
+            (top-left) convention. Default value is `OpenGL`.
+    """
+
+    def __init__(
+        self,
+        azimuth: Tensor | None = None,
+        elevation: Tensor | None = None,
+        degrees: bool = True,
+        spec_albedo: Tensor | None = None,
+        roughness: Tensor | None = None,
+        cameras: Camera | None = None,
+        lights: SgLightingParameters | None = None,
+        raw_texture: bool = True,
+        uvs_origin: Literal["OpenGL", "Kaolin"] = "OpenGL",
+    ) -> None:
+        super().__init__(cameras=cameras)
+        lights = lights if lights is not None else SgLightingParameters()
+        if azimuth is not None and elevation is not None:
+            azimuth = azimuth.view(-1, 3)
+            elevation = elevation.view(-1, 3)
+            if degrees:
+                azimuth = torch.pi / 180.0 * azimuth
+                elevation = torch.pi / 180.0 * elevation
+            direction = torch.stack(kal.ops.coords.spherical2cartesian(azimuth, elevation), dim=-1)
+            lights = SgLightingParameters(lights.amplitude, direction, lights.sharpness)
+        self._lights = lights
+        self._spec_albedo = spec_albedo if spec_albedo is not None else torch.ones(3)
+        self._roughness = roughness if roughness is not None else torch.ones(1)
+        self._get_albedo = _build_get_albedo(raw_texture, uvs_origin)
+        self._pre_hook = _register_uvs_and_normals
+        self._feature_dim = 5
+
+    def forward(self, fragments: Fragments, mesh: SurfaceMesh, **kwargs) -> Tensor:
+        """Render hard color shader with diffuse and specular (fitted SG) lighting from
+        rasterization outputs and mesh.
+
+        Args:
+            fragments: Rasterization outputs.
+            mesh: Batched representation of meshes to render.
+            kwargs: Optional overrides for rendering parameters. These include `cameras`, `lights`,
+                `spec_albedo` and `roughness`, all of which can override the values passed to the
+                constructor.
+
+        Returns:
+            Rendering output. Shape is (B, H, W, 3).
+        """
+        # apply overrides
+        cameras: Camera = kwargs.get("cameras", self._cameras)
+        lights: SgLightingParameters = kwargs.get("lights", self._lights)
+        spec_albedo: Tensor = kwargs.get("spec_albedo", self._spec_albedo).view(1, 1, 1, 3)
+        roughness: Tensor = kwargs.get("roughness", self._roughness).view(1, 1, 1)
+
+        # apply rendering
+        uv_map = fragments.features_image[1][..., :2]
+        normal_map = fragments.features_image[1][..., 2:]
+        albedo = self._get_albedo(uv_map, mesh)
+        mask = fragments.faces_image != -1
+        spec_albedo = spec_albedo.expand_as(albedo)
+        roughness = roughness.expand_as(mask)
+        _, rays_dir = generate_pinhole_rays_batched(cameras)
+        rays_dir = -rays_dir.view_as(albedo)
+        color = torch.zeros_like(albedo)
+        color[mask] += kal.render.lighting.sg_diffuse_fitted(
+            lights.amplitude, lights.direction, lights.sharpness, normal_map[mask], albedo[mask]
+        )
+        color[mask] += kal.render.lighting.sg_warp_specular_term(
+            lights.amplitude,
+            lights.direction,
+            lights.sharpness,
+            normal_map[mask],
+            roughness[mask],
+            rays_dir[mask],
+            spec_albedo[mask],
+        )
+        color[mask] = torch.clamp(color[mask], 0.0, 1.0)
+        return color
+
+    __call__ = forward
+
+    def to(self, device: str | device) -> "HardColorSpecularSGFittedShader":
+        if self._cameras is not None:
+            self._cameras = self._cameras.to(device=device)
+        if self._lights is not None:
+            self._lights = self._lights.to(device=device)
+        if self._spec_albedo is not None:
+            self._spec_albedo = self._spec_albedo.to(device=device)
+        if self._roughness is not None:
+            self._roughness = self._roughness.to(device=device)
+        return self
 
 
 # region Helpers
@@ -295,3 +410,8 @@ def _build_get_albedo(
 def _convert_raw_texture(texture: Tensor) -> Tensor:
     """Convert a raw uint8 (H, W, 3) texture to a float (3, H, W) texture."""
     return texture.float().permute(2, 0, 1) / 255.0
+
+
+def _register_uvs_and_normals(face_vertices_camera: Tensor, mesh: SurfaceMesh) -> Tensor:
+    """Pre-hook function for diffuse/specular shaders to register mesh uvs and normals."""
+    return torch.cat([mesh.face_uvs, mesh.face_normals], dim=-1)
--- vc_core/vc_core/dr/kaolin/utils.py
+++ vc_core/vc_core/dr/kaolin/utils.py
@@ -4,8 +4,11 @@ from typing import TYPE_CHECKING
 
 import kaolin as kal
 import torch
+from kaolin.render.camera import CameraFOV
+from kaolin.render.camera.raygen import _to_ndc_coords, generate_centered_pixel_coords
 
 if TYPE_CHECKING:
+    from kaolin.render.camera import Camera
     from torch import Tensor, device
 
 
@@ -44,3 +47,68 @@ def camera_position_from_spherical_angles(
     x, y, z = kal.ops.coords.spherical2cartesian(azim, elev, distance=dist)
     camera_position = torch.cat([x, y, z], dim=1)
     return camera_position
+
+
+def generate_pinhole_rays_batched(
+    cameras: Camera, coords_grid: Tensor | None = None
+) -> tuple[Tensor, Tensor]:
+    """Batched variant of the `generate_pinhole_rays` function in `kaolin.render.camera.raygen`.
+
+    For efficient batching, we generate rays only for one camera then apply each camera's
+    extrinsics to transform them to world frame.
+
+    This function assumes that the principal point (the pinhole location) is specified by a
+    displacement (camera.x0, camera.y0) in pixel coordinates from the center of the image.
+
+    The Kaolin camera class does not enforce a coordinate space for how the principal point is specified,
+    so users will need to make sure that the correct principal point conventions are followed for
+    the cameras passed into this function.
+
+    Args:
+        cameras: Batched camera object to generate rays for.
+        coords_grid: Pixel grid of ray-intersecting coordinates of shape `(H, W, 2)`. Coordinates
+            integer parts represent the pixel :math:`(i, j)` coords, and the fraction part of
+            `[0,1]` represents the location within the pixel itself. For example, a coordinate of
+            `(0.5, 0.5)` represents the center of the top-left pixel. Shared across all cameras.
+
+    Returns:
+        tuple of tensors representing the generated pinhole rays for the camera, as ray origins and
+            ray direction tensors respectively of shape `(B, HxW, 3)` each.
+    """
+    if coords_grid is None:
+        pixel_y, pixel_x = generate_centered_pixel_coords(
+            cameras.width, cameras.height, device=cameras.device
+        )
+    else:
+        assert cameras.device == coords_grid.device, (
+            f"Expected camera and coords_grid to be on the same device, "
+            f"but found {cameras.device} and {coords_grid.device}."
+        )
+        pixel_y, pixel_x = coords_grid[..., 0], coords_grid[..., 1]
+
+    # coords_grid should remain immutable (a new tensor is implicitly created here)
+    pixel_x = pixel_x.to(device=cameras.device, dtype=cameras.dtype)
+    pixel_y = pixel_y.to(device=cameras.device, dtype=cameras.dtype)
+
+    # Account for principal point (offsets from the center)
+    pixel_x = pixel_x - cameras.x0[0]
+    pixel_y = pixel_y + cameras.y0[0]
+
+    # pixel values are now in range [-1, 1], both tensors are of shape res_y x res_x
+    pixel_x, pixel_y = _to_ndc_coords(pixel_x, pixel_y, cameras)
+
+    ray_dir = torch.stack(
+        (
+            pixel_x * cameras.tan_half_fov(CameraFOV.HORIZONTAL)[0],
+            -pixel_y * cameras.tan_half_fov(CameraFOV.VERTICAL)[0],
+            -torch.ones_like(pixel_x),
+        ),
+        dim=-1,
+    )
+    ray_dir = ray_dir.reshape(-1, 3)  # Flatten grid rays to 1D array
+    ray_orig = torch.zeros_like(ray_dir)
+
+    # Transform from camera to world coordinates
+    ray_orig, ray_dir = cameras.extrinsics.inv_transform_rays(ray_orig, ray_dir)
+    ray_dir /= torch.linalg.norm(ray_dir, dim=-1, keepdim=True)
+    return ray_orig, ray_dir
